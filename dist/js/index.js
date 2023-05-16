58682c42e363f753f31c881391e0302c08205901
Mateus Borsoi <borsoimateus@gmail.com>
2023-05-15 22:29:49 -0300

    third

---
+++ dist/js/index.js
@@ -0,0 +1,24 @@
+"use strict";
+//Tipos: String, boolean, number, etc... 
+let x = 10;
+console.log(x);
+// Inferência X Annotation
+//Inferencia
+let y = 12;
+console.log(y);
+//Annotation
+let z = 15;
+console.log(z);
+//Tipos Básicos
+let fistname = "Mateus";
+let age = 24;
+const isAdmin = true;
+//object
+const pessoa = { nome: "Mateus", idade: 24 };
+console.log(pessoa);
+const numbers = [1, 2, 3, 4, 5, 6];
+console.log(numbers);
+//tuplas
+let mytuple;
+mytuple = [1, "Mateus", ["A", "B"]];
+console.log(mytuple);
---
+++ src/index.ts
@@ -0,0 +1,39 @@
+//Tipos: String, boolean, number, etc... 
+
+let x: number = 10
+console.log(x)
+
+// Inferência X Annotation
+
+//Inferencia
+
+let y = 12
+console.log(y)
+
+//Annotation
+
+let z: number = 15
+console.log(z)
+
+//Tipos Básicos
+
+let fistname: string = "Mateus"
+let age: number = 24
+const isAdmin: boolean = true
+
+//object
+
+const pessoa: object = {nome: "Mateus", idade: 24}
+console.log(pessoa)
+
+const numbers: number [] = [1,2,3,4,5,6]
+console.log(numbers)
+
+
+//tuplas
+
+let mytuple: [number, string, string[]] 
+
+mytuple = [1, "Mateus", ["A", "B"]]
+
+console.log(mytuple)
